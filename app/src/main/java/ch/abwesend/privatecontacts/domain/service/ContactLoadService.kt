b8a073aa93665353c23987ac4ebdac71631da748
Florian Gubler <florian.gubler@ergon.ch>
2021-12-27 11:16:39 +0100

    Added dummy-data for contacts

--- app/src/main/java/ch/abwesend/privatecontacts/domain/service/ContactLoadService.kt
+++ app/src/main/java/ch/abwesend/privatecontacts/domain/service/ContactLoadService.kt
@@ -1,5 +1,77 @@
 package ch.abwesend.privatecontacts.domain.service
 
-interface IContactLoadService
+import ch.abwesend.privatecontacts.domain.lib.flow.ResourceStateFlow
+import ch.abwesend.privatecontacts.domain.lib.flow.emitReady
+import ch.abwesend.privatecontacts.domain.lib.flow.mutableResourceStateFlow
+import ch.abwesend.privatecontacts.domain.model.Contact
+import ch.abwesend.privatecontacts.domain.model.ContactRead
+import ch.abwesend.privatecontacts.domain.model.PhoneNumber
+import ch.abwesend.privatecontacts.domain.model.PhoneNumberType
+import java.util.UUID
 
-class ContactLoadService : IContactLoadService
+interface IContactLoadService {
+    suspend fun loadContacts(): ResourceStateFlow<List<Contact>>
+}
+
+class ContactLoadService : IContactLoadService {
+    override suspend fun loadContacts(): ResourceStateFlow<List<Contact>> {
+        // TODO implement
+        val stateFlow = mutableResourceStateFlow<List<Contact>>()
+        stateFlow.emitReady(dummyContacts)
+        return stateFlow
+    }
+}
+
+private val dummyContacts = listOf(
+    ContactRead(
+        id = UUID.randomUUID(),
+        firstName = "Darth",
+        lastName = "Vader",
+        phoneNumbers = listOf(),
+    ),
+    ContactRead(
+        id = UUID.randomUUID(),
+        firstName = "Luke",
+        lastName = "Skywalker",
+        phoneNumbers = listOf(
+            PhoneNumber(
+                value = "1234",
+                type = PhoneNumberType.Private
+            ),
+        ),
+    ),
+    ContactRead(
+        id = UUID.randomUUID(),
+        firstName = "Obi-Wan",
+        lastName = "Kenobi",
+        phoneNumbers = listOf(
+            PhoneNumber(
+                value = "12345",
+                type = PhoneNumberType.Private
+            ),
+            PhoneNumber(
+                value = "123456",
+                type = PhoneNumberType.Business
+            ),
+        ),
+    ),
+    ContactRead(
+        id = UUID.randomUUID(),
+        firstName = "Yoda",
+        lastName = "",
+        phoneNumbers = listOf(
+            PhoneNumber(
+                value = "123456",
+                type = PhoneNumberType.Private
+            ),
+            PhoneNumber(
+                value = "1234567",
+                type = PhoneNumberType.Business
+            ),
+            PhoneNumber(
+                value = "12345678",
+                type = PhoneNumberType.Custom("Jedi-Number")
+            ),
+        ),
+    ),
+)
