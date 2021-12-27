ce62c3a0b8dd8707f286f69b5a6e27c6271004c4
Florian Gubler <florian.gubler@ergon.ch>
2021-12-27 15:20:18 +0100

    Log the source-class as well

--- app/src/main/java/ch/abwesend/privatecontacts/domain/lib/logging/LogcatLogger.kt
+++ app/src/main/java/ch/abwesend/privatecontacts/domain/lib/logging/LogcatLogger.kt
@@ -4,36 +4,40 @@ import android.util.Log
 
 class LogcatLogger(
     override val loggingTag: String,
-    override val logToCrashlytics: Boolean
+    override val logToCrashlytics: Boolean,
+    private val prefix: String? = null,
 ) : AbstractLogger() {
 
     override fun verboseImpl(messages: Collection<String>) {
         for (message in messages) {
-            Log.v(loggingTag, message)
+            Log.v(loggingTag, prefix(message))
         }
     }
 
     override fun debugImpl(messages: Collection<String>) {
         for (message in messages) {
-            Log.d(loggingTag, message)
+            Log.d(loggingTag, prefix(message))
         }
     }
 
     override fun infoImpl(messages: Collection<String>) {
         for (message in messages) {
-            Log.i(loggingTag, message)
+            Log.i(loggingTag, prefix(message))
         }
     }
 
     override fun warningImpl(messages: Collection<String>) {
         for (message in messages) {
-            Log.w(loggingTag, message)
+            Log.w(loggingTag, prefix(message))
         }
     }
 
     override fun errorImpl(messages: Collection<String>) {
         for (message in messages) {
-            Log.e(loggingTag, message)
+            Log.e(loggingTag, prefix(message))
         }
     }
+
+    private fun prefix(message: String) =
+        prefix?.let { "${it.take(30)}: $message" } ?: message
 }
--- app/src/main/java/ch/abwesend/privatecontacts/infrastructure/logging/LoggerFactory.kt
+++ app/src/main/java/ch/abwesend/privatecontacts/infrastructure/logging/LoggerFactory.kt
@@ -7,12 +7,12 @@ import ch.abwesend.privatecontacts.domain.lib.logging.LogcatLogger
 private const val LOGGING_TAG = "PrivateContacts"
 
 class LoggerFactory : ILoggerFactory {
-    private val logcatLogger: LogcatLogger by lazy {
-        LogcatLogger(LOGGING_TAG, logToCrashlytics = true)
-    }
-
     override fun createLogcat(callerClass: Class<*>): ILogger {
-        return logcatLogger
+        return LogcatLogger(
+            loggingTag = LOGGING_TAG,
+            logToCrashlytics = true,
+            prefix = callerClass.simpleName,
+        )
     }
 
     override fun createDefault(callerClass: Class<*>): ILogger =
