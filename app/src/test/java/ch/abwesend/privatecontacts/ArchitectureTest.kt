5103ca600aaf0c12ed720c67b0fa969461bc5294
Florian Gubler <florian.gubler@ergon.ch>
2021-12-29 09:05:13 +0100

    Added archunit tests

--- app/src/test/java/ch/abwesend/privatecontacts/ArchitectureTest.kt
+++ app/src/test/java/ch/abwesend/privatecontacts/ArchitectureTest.kt
@@ -4,6 +4,7 @@ import com.tngtech.archunit.core.importer.ImportOption
 import com.tngtech.archunit.junit.AnalyzeClasses
 import com.tngtech.archunit.junit.ArchTest
 import com.tngtech.archunit.lang.ArchRule
+import com.tngtech.archunit.lang.syntax.ArchRuleDefinition.noClasses
 import com.tngtech.archunit.library.Architectures
 
 // packages
@@ -12,6 +13,7 @@ private const val ROOT_PACKAGE = "ch.abwesend.privatecontacts"
 private const val APPLICATION_PACKAGE = "$ROOT_PACKAGE.application.."
 private const val VIEW_PACKAGE = "$ROOT_PACKAGE.view.."
 private const val DOMAIN_PACKAGE = "$ROOT_PACKAGE.domain.."
+private const val DOMAIN_LIB_PACKAGE = "$DOMAIN_PACKAGE.lib.."
 private const val INFRASTRUCTURE_PACKAGE = "$ROOT_PACKAGE.infrastructure.."
 
 // layers
@@ -27,14 +29,29 @@ private const val INFRASTRUCTURE_LAYER = "INFRASTRUCTURE"
 )
 class ArchitectureTest {
     @ArchTest
-    val `view layer may not be accessed by any other layers`: ArchRule = layers()
+    val `view layer may only be accessed by application`: ArchRule = layers()
         .whereLayer(VIEW_LAYER)
         .mayOnlyBeAccessedByLayers(APPLICATION_LAYER)
 
     @ArchTest
-    val `infrastructure layer may not be accessed by any other layers`: ArchRule = layers()
+    val `infrastructure layer may only be accessed by application`: ArchRule = layers()
         .whereLayer(INFRASTRUCTURE_LAYER)
         .mayOnlyBeAccessedByLayers(APPLICATION_LAYER)
+
+    @ArchTest
+    val `application layer may not be accessed by any other layers`: ArchRule = layers()
+        .whereLayer(APPLICATION_LAYER)
+        .mayNotBeAccessedByAnyLayer()
+
+    @ArchTest
+    val `only infrastructure layer may access entities`: ArchRule = noClasses()
+        .that().resideOutsideOfPackage(INFRASTRUCTURE_PACKAGE)
+        .should().dependOnClassesThat().haveSimpleNameEndingWith("Entity")
+
+    @ArchTest
+    val `only infrastructure layer may access daos`: ArchRule = noClasses()
+        .that().resideOutsideOfPackage(INFRASTRUCTURE_PACKAGE)
+        .should().dependOnClassesThat().haveSimpleNameEndingWith("Dao")
 
     private fun layers() = Architectures.layeredArchitecture()
         .layer(APPLICATION_LAYER).definedBy(APPLICATION_PACKAGE)
