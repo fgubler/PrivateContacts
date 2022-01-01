c5f3413a433c6b5733d2f4cdad355180d508d410
Florian Gubler <florian.gubler@ergon.ch>
2022-01-01 12:14:41 +0100

    Refactoring

--- app/src/main/java/ch/abwesend/privatecontacts/application/PrivateContactsApplication.kt
+++ app/src/main/java/ch/abwesend/privatecontacts/application/PrivateContactsApplication.kt
@@ -2,6 +2,7 @@ package ch.abwesend.privatecontacts.application
 
 import android.app.Application
 import android.content.Context
+import ch.abwesend.privatecontacts.domain.lib.logging.LogcatLogger
 import org.koin.android.ext.koin.androidContext
 import org.koin.android.ext.koin.androidLogger
 import org.koin.core.component.KoinComponent
@@ -11,11 +12,20 @@ import org.koin.core.logger.Level
 class PrivateContactsApplication : Application(), KoinComponent {
     override fun onCreate() {
         super.onCreate()
+        initializeKoin()
+    }
+
+    private fun initializeKoin() {
         val context: Context = this
 
         startKoin {
-            // TODO for some reason, it crashes with a lower one: see https://github.com/InsertKoinIO/koin/issues/1188
-            androidLogger(Level.ERROR) // use android logger for koin-internal logging
+            try {
+                // TODO for some reason, it crashes with a lower one: see https://github.com/InsertKoinIO/koin/issues/1188
+                androidLogger(Level.ERROR) // use android logger for koin-internal logging
+            } catch (e: Exception) {
+                LogcatLogger("PrivateContactsApplication", true)
+                    .error("Failed to register android logger on koin", e)
+            }
             androidContext(context)
             modules(koinModule)
         }
