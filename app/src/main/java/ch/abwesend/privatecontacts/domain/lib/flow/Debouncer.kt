822fbe17f0c6e85c4dc52ac1bc9c20054857ba77
Florian Gubler <florian.gubler@ergon.ch>
2022-02-11 16:14:03 +0100

    Created debouncer for user-inputs

---
+++ app/src/main/java/ch/abwesend/privatecontacts/domain/lib/flow/Debouncer.kt
@@ -0,0 +1,39 @@
+package ch.abwesend.privatecontacts.domain.lib.flow
+
+import ch.abwesend.privatecontacts.domain.lib.coroutine.IDispatchers
+import ch.abwesend.privatecontacts.domain.util.getAnywhere
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.FlowPreview
+import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.flow.debounce
+import kotlinx.coroutines.launch
+
+@FlowPreview
+class Debouncer<T>(private val scope: CoroutineScope, debounceMs: Long) {
+    private val flow = MutableSharedFlow<T>()
+    private val debouncedFlow = flow.debounce(debounceMs)
+
+    fun newValue(value: T) {
+        scope.launch { flow.emit(value) }
+    }
+
+    companion object {
+        fun <T> debounce(
+            scope: CoroutineScope,
+            debounceMs: Long,
+            action: (T) -> Unit
+        ): Debouncer<T> {
+            val debouncer = Debouncer<T>(scope, debounceMs)
+
+            val dispatchers: IDispatchers = getAnywhere()
+            scope.launch(dispatchers.default) {
+                debouncer.debouncedFlow.collect { value ->
+                    action(value)
+                }
+            }
+
+            return debouncer
+        }
+    }
+}
