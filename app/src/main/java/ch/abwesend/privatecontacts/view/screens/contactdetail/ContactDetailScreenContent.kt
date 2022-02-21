f9ab9e9ab7174b186630b55613971e7c7b644e17
Florian Gubler <florian.gubler@gmail.com>
2022-02-21 08:44:54 +0100

    Updated Copyrights

--- app/src/main/java/ch/abwesend/privatecontacts/view/screens/contactdetail/ContactDetailScreenContent.kt
+++ app/src/main/java/ch/abwesend/privatecontacts/view/screens/contactdetail/ContactDetailScreenContent.kt
@@ -1,3 +1,9 @@
+/*
+ * Private Contacts
+ * Copyright (c) 2022.
+ * Florian Gubler
+ */
+
 package ch.abwesend.privatecontacts.view.screens.contactdetail
 
 import androidx.compose.foundation.layout.padding
