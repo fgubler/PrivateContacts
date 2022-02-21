25572c7e7708de6fd9dae98c665bc18f2d4f56e6
Florian Gubler <florian.gubler@gmail.com>
2022-02-21 15:33:46 +0100

    Use try-catch instead of checking beforehand

--- app/src/main/java/ch/abwesend/privatecontacts/view/util/IntentHelper.kt
+++ app/src/main/java/ch/abwesend/privatecontacts/view/util/IntentHelper.kt
@@ -13,12 +13,18 @@ import androidx.core.content.ContextCompat
 import ch.abwesend.privatecontacts.R
 import ch.abwesend.privatecontacts.domain.lib.logging.logger
 
+/**
+ * It seems, a try-catch is better than checking beforehand because
+ * there are some data-protection rules which might interfere.
+ * See also https://developer.android.com/training/package-visibility
+ */
 fun Intent.tryStartActivity(context: Context) {
     logger.debug("Trying to start intent to '$data'")
-    if (resolveActivity(context.packageManager) != null) {
+
+    try {
         ContextCompat.startActivity(context, this, null)
-    } else {
-        logger.warning("No app found for schema '${data?.scheme}'")
+    } catch(t: Throwable) {
+        logger.warning("No app found for schema '${data?.scheme}'", t)
         Toast
             .makeText(context, R.string.deeplink_failed_no_appropriate_app, Toast.LENGTH_SHORT)
             .show()
