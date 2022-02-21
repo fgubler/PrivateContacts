4785e22222969c0d39287c80d86bba9703c8df6f
Florian Gubler <florian.gubler@gmail.com>
2022-02-21 14:34:47 +0100

    Added an error-message in case no app is found

--- app/src/main/java/ch/abwesend/privatecontacts/view/util/IntentHelper.kt
+++ app/src/main/java/ch/abwesend/privatecontacts/view/util/IntentHelper.kt
@@ -8,10 +8,18 @@ package ch.abwesend.privatecontacts.view.util
 
 import android.content.Context
 import android.content.Intent
+import android.widget.Toast
 import androidx.core.content.ContextCompat
+import ch.abwesend.privatecontacts.R
+import ch.abwesend.privatecontacts.domain.lib.logging.logger
 
 fun Intent.tryStartActivity(context: Context) {
+    logger.debug("Trying to start intent to '$data'")
     if (resolveActivity(context.packageManager) != null) {
         ContextCompat.startActivity(context, this, null)
+    } else {
+        Toast
+            .makeText(context, R.string.deeplink_failed_no_appropriate_app, Toast.LENGTH_SHORT)
+            .show()
     }
 }
