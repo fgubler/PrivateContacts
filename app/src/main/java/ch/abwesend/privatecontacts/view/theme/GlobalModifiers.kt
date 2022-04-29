1e7af7484b8b130b551f6a36f84c5f64ced0cc1c
Florian Gubler <florian.gubler@gmail.com>
2022-04-29 16:20:28 +0200

    Added concept of global modifiers

---
+++ app/src/main/java/ch/abwesend/privatecontacts/view/theme/GlobalModifiers.kt
@@ -0,0 +1,15 @@
+/*
+ * Private Contacts
+ * Copyright (c) 2022.
+ * Florian Gubler
+ */
+
+package ch.abwesend.privatecontacts.view.theme
+
+import androidx.compose.foundation.layout.heightIn
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+
+object GlobalModifiers {
+    val buttonHeightLarge = Modifier.heightIn(min = 50.dp)
+}
