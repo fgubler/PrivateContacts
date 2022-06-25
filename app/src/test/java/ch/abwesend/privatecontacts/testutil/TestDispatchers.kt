3df3b8ae2507aaaeee959a9e848735672d73d2bc
Florian Gubler <florian.gubler@gmail.com>
2022-06-25 16:13:13 +0200

    Fixed tests

--- app/src/test/java/ch/abwesend/privatecontacts/testutil/TestDispatchers.kt
+++ app/src/test/java/ch/abwesend/privatecontacts/testutil/TestDispatchers.kt
@@ -10,11 +10,16 @@ import ch.abwesend.privatecontacts.domain.lib.coroutine.IDispatchers
 import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.test.StandardTestDispatcher
+import kotlinx.coroutines.test.UnconfinedTestDispatcher
 
+/**
+ * Beware: do not use [StandardTestDispatcher].
+ * Otherwise, the build will hang itself during the 56t test (whichever that may be).
+ */
 @ExperimentalCoroutinesApi
 object TestDispatchers : IDispatchers {
-    override val default: CoroutineDispatcher = StandardTestDispatcher()
-    override val io: CoroutineDispatcher = StandardTestDispatcher()
-    override val main: CoroutineDispatcher = StandardTestDispatcher()
-    override val mainImmediate: CoroutineDispatcher = StandardTestDispatcher()
+    override val default: CoroutineDispatcher = UnconfinedTestDispatcher()
+    override val io: CoroutineDispatcher = UnconfinedTestDispatcher()
+    override val main: CoroutineDispatcher = UnconfinedTestDispatcher()
+    override val mainImmediate: CoroutineDispatcher = UnconfinedTestDispatcher()
 }
