e18baeeafb0e9b22a9f3b760812f9d179d6de0e8
Florian Gubler <florian.gubler@ergon.ch>
2022-08-05 13:27:46 +0200

    Moved StringBasedContactData to its own file

--- app/src/main/java/ch/abwesend/privatecontacts/domain/model/contactdata/ContactData.kt
+++ app/src/main/java/ch/abwesend/privatecontacts/domain/model/contactdata/ContactData.kt
@@ -27,33 +27,3 @@ sealed interface ContactData {
     /** returning null means that the data will be ignored in search */
     fun formatValueForSearch(): String?
 }
-
-interface StringBasedContactData : ContactData {
-    /** Raw value */
-    val value: String
-    /** Formatted for display */
-    val formattedValue: String get() = value
-    /** Formatted for automatic matching */
-    val valueForMatching: String get() = value
-
-    override val isEmpty: Boolean
-        get() = value.isEmpty()
-
-    override val displayValue: String
-        get() = formattedValue
-
-    override fun formatValueForSearch(): String = formatValueForSearch(value)
-
-    companion object {
-        fun formatValueForSearch(value: String): String = value
-    }
-}
-
-/**
- * The generics are needed to make the functions return the dynamic type of "this"
- */
-interface StringBasedContactDataGeneric<T : StringBasedContactDataGeneric<T>> : StringBasedContactData {
-    fun changeValue(value: String): T
-    override fun changeType(type: ContactDataType): T
-    override fun delete(): T
-}
