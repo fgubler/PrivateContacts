0d82bb2a2329dbef7d4452751182b10bb84fb031
Florian Gubler <florian.gubler@ergon.ch>
2022-08-12 17:02:44 +0200

    Added relation-class for contacts and contact-groups

--- app/src/main/java/ch/abwesend/privatecontacts/infrastructure/room/database/AppDatabase.kt
+++ app/src/main/java/ch/abwesend/privatecontacts/infrastructure/room/database/AppDatabase.kt
@@ -14,16 +14,26 @@ import ch.abwesend.privatecontacts.infrastructure.room.contact.ContactDao
 import ch.abwesend.privatecontacts.infrastructure.room.contact.ContactEntity
 import ch.abwesend.privatecontacts.infrastructure.room.contactdata.ContactDataDao
 import ch.abwesend.privatecontacts.infrastructure.room.contactdata.ContactDataEntity
+import ch.abwesend.privatecontacts.infrastructure.room.contactgroup.ContactGroupDao
+import ch.abwesend.privatecontacts.infrastructure.room.contactgroup.ContactGroupEntity
+import ch.abwesend.privatecontacts.infrastructure.room.contactgrouprelation.ContactGroupRelationDao
+import ch.abwesend.privatecontacts.infrastructure.room.contactgrouprelation.ContactGroupRelationEntity
 import java.util.concurrent.atomic.AtomicBoolean
 
 @Database(
-    version = 21,
+    version = 22,
     exportSchema = true,
-    entities = [ContactEntity::class, ContactDataEntity::class],
+    entities = [
+        ContactEntity::class,
+        ContactDataEntity::class,
+        ContactGroupEntity::class,
+        ContactGroupRelationEntity::class,
+    ],
     autoMigrations = [
         AutoMigration(from = 18, to = 19),
         AutoMigration(from = 19, to = 20),
         AutoMigration(from = 20, to = 21),
+        AutoMigration(from = 21, to = 22),
     ]
 )
 @TypeConverters(AppTypeConverters::class)
@@ -33,4 +43,6 @@ abstract class AppDatabase : RoomDatabase() {
 
     abstract fun contactDao(): ContactDao
     abstract fun contactDataDao(): ContactDataDao
+    abstract fun contactGroupDao(): ContactGroupDao
+    abstract fun contactGroupRelationDao(): ContactGroupRelationDao
 }
