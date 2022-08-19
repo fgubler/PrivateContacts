981322e667d04cc4fc5aeb573d83aaef1927865c
Florian Gubler <florian.gubler@ergon.ch>
2022-08-19 16:21:13 +0200

    WIP: first draft for implementing delete-logic

--- app/src/main/java/ch/abwesend/privatecontacts/infrastructure/repository/androidcontacts/repository/AndroidContactSaveRepository.kt
+++ app/src/main/java/ch/abwesend/privatecontacts/infrastructure/repository/androidcontacts/repository/AndroidContactSaveRepository.kt
@@ -1,11 +1,26 @@
 package ch.abwesend.privatecontacts.infrastructure.repository.androidcontacts.repository
 
+import ch.abwesend.privatecontacts.domain.lib.logging.logger
 import ch.abwesend.privatecontacts.domain.model.contact.IContactIdExternal
+import ch.abwesend.privatecontacts.domain.model.result.ContactChangeError.UNKNOWN_ERROR
 import ch.abwesend.privatecontacts.domain.model.result.ContactDeleteResult
 import ch.abwesend.privatecontacts.domain.repository.IAndroidContactSaveRepository
 
+// TODO consider whether to really catch exceptions here
 class AndroidContactSaveRepository : AndroidContactRepositoryBase(), IAndroidContactSaveRepository {
-    override suspend fun deleteContacts(contactIds: List<IContactIdExternal>): ContactDeleteResult {
-        TODO("Implement")
-    }
+    override suspend fun deleteContacts(contactIds: List<IContactIdExternal>): ContactDeleteResult =
+        try {
+            checkContactWritePermission { exception -> throw exception }
+
+            withContactStore { contactStore ->
+                contactStore.execute {
+                    contactIds.forEach { delete(it.contactNo) }
+                }
+            }
+
+            ContactDeleteResult.Success
+        } catch (t: Throwable) {
+            logger.error("Failed to delete contacts $contactIds", t)
+            ContactDeleteResult.Failure(UNKNOWN_ERROR)
+        }
 }
