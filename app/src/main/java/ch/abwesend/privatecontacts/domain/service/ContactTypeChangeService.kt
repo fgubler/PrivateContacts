9596051ab452c5cd3f641bd0fd4d1efe0e599836
Florian Gubler <florian.gubler@ergon.ch>
2022-10-21 14:07:57 +0200

    Added batch-processing for changing a contact-type

--- app/src/main/java/ch/abwesend/privatecontacts/domain/service/ContactTypeChangeService.kt
+++ app/src/main/java/ch/abwesend/privatecontacts/domain/service/ContactTypeChangeService.kt
@@ -6,6 +6,8 @@
 
 package ch.abwesend.privatecontacts.domain.service
 
+import ch.abwesend.privatecontacts.domain.lib.coroutine.Dispatchers
+import ch.abwesend.privatecontacts.domain.lib.coroutine.mapAsync
 import ch.abwesend.privatecontacts.domain.lib.logging.logger
 import ch.abwesend.privatecontacts.domain.model.ModelStatus
 import ch.abwesend.privatecontacts.domain.model.ModelStatus.DELETED
@@ -15,16 +17,42 @@ import ch.abwesend.privatecontacts.domain.model.contact.ContactType
 import ch.abwesend.privatecontacts.domain.model.contact.IContactBase
 import ch.abwesend.privatecontacts.domain.model.contact.IContactEditable
 import ch.abwesend.privatecontacts.domain.model.contact.toContactBase
+import ch.abwesend.privatecontacts.domain.model.result.ContactBatchChangeResult
 import ch.abwesend.privatecontacts.domain.model.result.ContactChangeError.NOT_YET_IMPLEMENTED_FOR_INTERNAL_CONTACTS
 import ch.abwesend.privatecontacts.domain.model.result.ContactChangeError.UNABLE_TO_CREATE_CONTACT_WITH_NEW_TYPE
 import ch.abwesend.privatecontacts.domain.model.result.ContactChangeError.UNABLE_TO_DELETE_CONTACT_WITH_OLD_TYPE
 import ch.abwesend.privatecontacts.domain.model.result.ContactDeleteResult
 import ch.abwesend.privatecontacts.domain.model.result.ContactSaveResult
+import ch.abwesend.privatecontacts.domain.model.result.ContactSaveResult.Failure
+import ch.abwesend.privatecontacts.domain.model.result.ContactSaveResult.Success
 import ch.abwesend.privatecontacts.domain.model.result.ContactSaveResult.ValidationFailure
 import ch.abwesend.privatecontacts.domain.util.injectAnywhere
+import kotlinx.coroutines.withContext
 
 class ContactTypeChangeService {
     private val saveService: ContactSaveService by injectAnywhere()
+    private val dispatchers: Dispatchers by injectAnywhere()
+
+    // TODO add proper batch processing
+    // TODO consider whether we really want to do it in parallel or if it is too risky
+    suspend fun changeContactType(
+        contacts: Collection<IContactEditable>,
+        newType: ContactType
+    ): ContactBatchChangeResult = withContext(dispatchers.default) {
+        val partialResults = contacts
+            .mapAsync { contact -> contact.id to changeContactType(contact, newType) }
+            .toMap()
+
+        val successfulContacts = partialResults.filterValues { it is Success }.map { it.key }.toSet()
+        val failedContacts = partialResults.keys.minus(successfulContacts)
+
+        logger.debug("Changed contact-type: ${successfulContacts.size} successful, ${failedContacts.size} failed.")
+
+        ContactBatchChangeResult(
+            successfulChanges = successfulContacts.toList(),
+            failedChanges = failedContacts.toList(),
+        )
+    }
 
     suspend fun changeContactType(contact: IContactEditable, newType: ContactType): ContactSaveResult {
         logger.debug("Trying to change contact-type from ${contact.type} to $newType")
@@ -34,7 +62,7 @@ class ContactTypeChangeService {
                 logger.debug("No contact-type change necessary: just save it normally.")
                 saveService.saveContact(contact)
             }
-            ContactType.PUBLIC -> ContactSaveResult.Failure(NOT_YET_IMPLEMENTED_FOR_INTERNAL_CONTACTS)
+            ContactType.PUBLIC -> Failure(NOT_YET_IMPLEMENTED_FOR_INTERNAL_CONTACTS)
             ContactType.SECRET -> changeContactTypeToSecret(contact, newType)
         }
     }
@@ -54,12 +82,12 @@ class ContactTypeChangeService {
                 logger.warning("Failed to save contact due to validation")
                 saveResult
             }
-            is ContactSaveResult.Failure -> {
+            is Failure -> {
                 logger.warning("Failed to save contact ${oldContact.id} with new type $newType")
                 val errors = listOf(UNABLE_TO_CREATE_CONTACT_WITH_NEW_TYPE)
-                ContactSaveResult.Failure(errors + saveResult.errors)
+                Failure(errors + saveResult.errors)
             }
-            is ContactSaveResult.Success -> {
+            is Success -> {
                 logger.debug("Successfully saved contact with new type $newType")
                 deleteContactWithOldType(oldContact)
             }
@@ -88,11 +116,11 @@ class ContactTypeChangeService {
             is ContactDeleteResult.Failure -> {
                 logger.warning("Failed to delete old contact with id ${contact.id}")
                 val errors = listOf(UNABLE_TO_DELETE_CONTACT_WITH_OLD_TYPE)
-                ContactSaveResult.Failure(errors + deleteResult.errors)
+                Failure(errors + deleteResult.errors)
             }
             ContactDeleteResult.Success -> {
                 logger.debug("Deleted old contact with id ${contact.id}")
-                ContactSaveResult.Success
+                Success
             }
         }
     }
