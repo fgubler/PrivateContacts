f344fa287d282b5caf0d7e7f476a800f792d6e29
Florian Gubler <florian.gubler@gmail.com>
2022-11-19 18:58:34 +0100

    Replace + with 00 for search

---
+++ app/src/test/java/ch/abwesend/privatecontacts/domain/model/contactdata/PhoneNumberTest.kt
@@ -0,0 +1,39 @@
+/*
+ * Private Contacts
+ * Copyright (c) 2022.
+ * Florian Gubler
+ */
+
+package ch.abwesend.privatecontacts.domain.model.contactdata
+
+import ch.abwesend.privatecontacts.testutil.TestBase
+import ch.abwesend.privatecontacts.testutil.databuilders.someEventDate
+import ch.abwesend.privatecontacts.testutil.databuilders.somePhoneNumber
+import io.mockk.junit5.MockKExtension
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.extension.ExtendWith
+import java.time.LocalDate
+
+@ExperimentalCoroutinesApi
+@ExtendWith(MockKExtension::class)
+class PhoneNumberTest : TestBase() {
+    @Test
+    fun `should replace + from the prefix with 00 for search`() {
+        val underTest = somePhoneNumber(value = "+41 44 123 45 67")
+
+        val result = underTest.formatValueForSearch()
+
+        assertThat(result).isEqualTo("0041441234567")
+    }
+
+    @Test
+    fun `should remove all other non-digit characters`() {
+        val underTest = somePhoneNumber(value = "+41+44-123:4_5ABC6?7")
+
+        val result = underTest.formatValueForSearch()
+
+        assertThat(result).isEqualTo("0041441234567")
+    }
+}
