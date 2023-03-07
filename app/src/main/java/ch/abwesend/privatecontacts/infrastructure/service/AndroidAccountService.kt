d3c5e14ffd20e78a5cc30cde8f700892d1f2c6cb
Florian Gubler <florian.gubler@gmail.com>
2023-03-07 19:03:32 +0100

    Make sure that the list of possible accounts is never empty

--- app/src/main/java/ch/abwesend/privatecontacts/infrastructure/service/AndroidAccountService.kt
+++ app/src/main/java/ch/abwesend/privatecontacts/infrastructure/service/AndroidAccountService.kt
@@ -21,18 +21,19 @@ class AndroidAccountService(private val context: Context) : AccountService {
 
     private val knownAccountProviders: Set<String> = setOf(ACCOUNT_PROVIDER_GOOGLE) // TODO extend
 
-    override fun loadAvailableAccounts(): List<ContactAccount> =
-        if (!permissionService.hasReadAccountsPermission()) {
+    override fun loadAvailableAccounts(): List<ContactAccount> {
+        val onlineAccounts: List<ContactAccount> = if (!permissionService.hasReadAccountsPermission()) {
             logger.warning("Missing permission to load accounts: cannot suggest more than local-contacts.")
             emptyList()
         } else {
             val accounts = AccountManager.get(context).accounts
-            val onlineAccounts: List<ContactAccount> = accounts
+            accounts
                 .map { OnlineAccount(username = it.name, accountProvider = it.type) }
                 .also { logger.debug("Found ${it.size} accounts") }
                 .filter { knownAccountProviders.contains(it.accountProvider) }
                 .also { logger.debug("Found ${it.size} accounts of known providers") }
                 .sortedBy { it.username } // just to make the order constant
-            onlineAccounts + ContactAccount.LocalPhoneContacts
         }
+        return onlineAccounts + ContactAccount.LocalPhoneContacts
+    }
 }
