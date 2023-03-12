54691689fc34b182ead8a458a5c69318bb329373
Florian Gubler <florian.gubler@gmail.com>
2023-03-12 11:09:50 +0100

    Removed deprecated paging logic

--- app/src/main/java/ch/abwesend/privatecontacts/application/KoinModule.kt
+++ app/src/main/java/ch/abwesend/privatecontacts/application/KoinModule.kt
@@ -10,7 +10,6 @@ import ch.abwesend.privatecontacts.domain.lib.coroutine.ApplicationScope
 import ch.abwesend.privatecontacts.domain.lib.coroutine.Dispatchers
 import ch.abwesend.privatecontacts.domain.lib.coroutine.IDispatchers
 import ch.abwesend.privatecontacts.domain.lib.logging.ILoggerFactory
-import ch.abwesend.privatecontacts.domain.repository.ContactPagerFactory
 import ch.abwesend.privatecontacts.domain.repository.IAddressFormattingRepository
 import ch.abwesend.privatecontacts.domain.repository.IAndroidContactLoadService
 import ch.abwesend.privatecontacts.domain.repository.IAndroidContactSaveService
@@ -33,7 +32,6 @@ import ch.abwesend.privatecontacts.domain.settings.SettingsRepository
 import ch.abwesend.privatecontacts.infrastructure.calldetection.CallNotificationRepository
 import ch.abwesend.privatecontacts.infrastructure.calldetection.IncomingCallHelper
 import ch.abwesend.privatecontacts.infrastructure.logging.LoggerFactory
-import ch.abwesend.privatecontacts.infrastructure.paging.ContactPagingSource
 import ch.abwesend.privatecontacts.infrastructure.repository.ContactDataRepository
 import ch.abwesend.privatecontacts.infrastructure.repository.ContactGroupRepository
 import ch.abwesend.privatecontacts.infrastructure.repository.ContactImageRepository
@@ -109,9 +107,6 @@ internal val koinModule = module {
     // Factories
     single<ILoggerFactory> { LoggerFactory() }
     single<IDatabaseFactory<AppDatabase>> { DatabaseFactory() }
-
-    @Suppress("DEPRECATION")
-    single<ContactPagerFactory> { ContactPagingSource.Companion }
 
     // Helpers
     single { IncomingCallHelper() }
