6a343d795f1ab8631924f1768fa3fe441bb83f88
Florian Gubler <florian.gubler@gmail.com>
2023-03-12 11:31:37 +0100

    let tests rethrow to get better analysis why they fail on github

--- app/src/test/java/ch/abwesend/privatecontacts/infrastructure/repository/androidcontacts/AndroidContactSaveServiceTest.kt
+++ app/src/test/java/ch/abwesend/privatecontacts/infrastructure/repository/androidcontacts/AndroidContactSaveServiceTest.kt
@@ -185,7 +185,7 @@ class AndroidContactSaveServiceTest : TestBase() {
         assertThat(capturedContact.nickname).isEqualTo(androidContact.nickname)
         assertThat(capturedContact.note?.raw).isEqualTo(androidContact.note?.raw)
         // contact-data
-        val capturedContactTransformed = capturedContact.toContact(emptyList(), rethrowExceptions = false)
+        val capturedContactTransformed = capturedContact.toContact(emptyList(), rethrowExceptions = true)
         assertThat(capturedContactTransformed).isNotNull
         assertContactDataEquals(changedContact, capturedContactTransformed!!)
     }
@@ -224,7 +224,7 @@ class AndroidContactSaveServiceTest : TestBase() {
         assertThat(capturedContact.lastName).isEqualTo(newContact.lastName)
         assertThat(capturedContact.nickname).isEqualTo(newContact.nickname)
         assertThat(capturedContact.note?.raw).isEqualTo(newContact.notes)
-        val capturedContactTransformed = capturedContact.toContact(groups = emptyList(), rethrowExceptions = false)
+        val capturedContactTransformed = capturedContact.toContact(groups = emptyList(), rethrowExceptions = true)
         assertThat(capturedContactTransformed).isNotNull
         assertContactDataEquals(newContact, capturedContactTransformed!!)
     }
