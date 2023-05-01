0d720d3ec1d9ecb7103c942df5da0f7e4b96eddf
Florian Gubler <florian.gubler@gmail.com>
2023-05-01 20:55:38 +0200

    Added generic Result class

---
+++ app/src/main/java/ch/abwesend/privatecontacts/domain/model/result/Result.kt
@@ -0,0 +1,28 @@
+/*
+ * Private Contacts
+ * Copyright (c) 2023.
+ * Florian Gubler
+ */
+
+package ch.abwesend.privatecontacts.domain.model.result
+
+sealed interface Result<TValue, TError> {
+    fun getValueOrNull(): TValue?
+    fun <T> mapValue(mapper: (TValue) -> T): Result<T, TError>
+
+    data class Success<TValue, TError>(val value: TValue) : BinaryResult<TValue, TError> {
+        override fun getValueOrNull(): TValue? = value
+
+        override fun <T> mapValue(mapper: (TValue) -> T): Result<T, TError> {
+            val newValue = mapper(value)
+            return Success(newValue)
+        }
+    }
+
+    data class Error<TValue, TError>(val error: TError) : BinaryResult<TValue, TError> {
+        override fun getValueOrNull(): TValue? = null
+        override fun <T> mapValue(mapper: (TValue) -> T): Result<T, TError> = Error(error)
+    }
+}
+
+sealed interface BinaryResult<TValue, TError> : Result<TValue, TError>
