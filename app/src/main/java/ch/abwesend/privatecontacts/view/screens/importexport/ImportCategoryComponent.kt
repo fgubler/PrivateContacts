ecf4cc3bdf0103bb49725285bfd21f64328426b7
Florian Gubler <florian.gubler@gmail.com>
2023-05-28 10:19:37 +0200

    Extracted common logic and made small improvements

--- app/src/main/java/ch/abwesend/privatecontacts/view/screens/importexport/ImportCategoryComponent.kt
+++ app/src/main/java/ch/abwesend/privatecontacts/view/screens/importexport/ImportCategoryComponent.kt
@@ -7,37 +7,30 @@
 package ch.abwesend.privatecontacts.view.screens.importexport
 
 import android.net.Uri
-import androidx.activity.compose.ManagedActivityResultLauncher
-import androidx.activity.compose.rememberLauncherForActivityResult
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.height
 import androidx.compose.material.ExperimentalMaterialApi
-import androidx.compose.material.OutlinedTextField
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import ch.abwesend.privatecontacts.R
-import ch.abwesend.privatecontacts.domain.lib.logging.logger
 import ch.abwesend.privatecontacts.domain.model.contact.ContactAccount
 import ch.abwesend.privatecontacts.domain.model.contact.ContactType
 import ch.abwesend.privatecontacts.domain.model.contact.ContactType.PUBLIC
 import ch.abwesend.privatecontacts.domain.model.contact.ContactType.SECRET
-import ch.abwesend.privatecontacts.view.components.buttons.EditIconButton
 import ch.abwesend.privatecontacts.view.components.buttons.SecondaryButton
 import ch.abwesend.privatecontacts.view.components.inputs.AccountSelectionDropDownField
 import ch.abwesend.privatecontacts.view.components.inputs.ContactTypeField
+import ch.abwesend.privatecontacts.view.components.inputs.OpenFileFilePicker
 import ch.abwesend.privatecontacts.view.screens.importexport.ImportExportScreenComponents.ImportExportCategory
-import ch.abwesend.privatecontacts.view.screens.importexport.extensions.OpenDocumentContract
+import ch.abwesend.privatecontacts.view.screens.importexport.extensions.ImportExportConstants.VCF_MIME_TYPES
 import ch.abwesend.privatecontacts.view.screens.importexport.extensions.getFilePathForDisplay
 import ch.abwesend.privatecontacts.view.viewmodel.ImportExportViewModel
 import kotlin.contracts.ExperimentalContracts
@@ -45,7 +38,6 @@ import kotlin.contracts.ExperimentalContracts
 @ExperimentalMaterialApi
 @ExperimentalContracts
 object ImportCategoryComponent {
-    private val VCF_MIME_TYPES = arrayOf("text/vcard", "text/x-vcard")
     private val parent = ImportExportScreen // TODO remove once google issue 212091796 is fixed
 
     @Composable
@@ -58,14 +50,17 @@ object ImportCategoryComponent {
 
         ImportExportCategory(title = R.string.import_title) {
             VcfFilePicker(filePath) { uri ->
-                viewModel.setImportFile(uri)
+                // in the case of "cancel", leave the old value
+                uri?.let { viewModel.setImportFile(it) }
             }
 
             Spacer(modifier = Modifier.height(10.dp))
 
-            ContactTypeField(selectedType = targetType, isScrolling = { parent.isScrolling }) { newType ->
-                targetType = newType
-            }
+            ContactTypeField(
+                labelRes = R.string.import_contacts_as,
+                selectedType = targetType,
+                isScrolling = { parent.isScrolling }
+            ) { newType -> targetType = newType }
 
             when (targetType) {
                 SECRET -> Unit
@@ -93,28 +88,11 @@ object ImportCategoryComponent {
 
     @Composable
     private fun VcfFilePicker(selectedFilePath: String, onFileSelected: (Uri?) -> Unit) {
-        val launcher = rememberLauncherForActivityResult(
-            contract = OpenDocumentContract(),
-            onResult = onFileSelected,
+        OpenFileFilePicker(
+            labelRes = R.string.select_file_to_import,
+            mimeTypes = VCF_MIME_TYPES,
+            selectedFilePath = selectedFilePath,
+            onFileSelected = onFileSelected,
         )
-
-        Row(verticalAlignment = Alignment.CenterVertically) {
-            OutlinedTextField(
-                label = { Text(text = stringResource(id = R.string.select_file_to_import)) },
-                value = selectedFilePath,
-                enabled = false,
-                modifier = Modifier
-                    .weight(1f)
-                    .clickable { editImportFilePath(launcher) },
-                onValueChange = { newValue ->
-                    logger.debug("Changed import file path to $newValue")
-                }
-            )
-            EditIconButton { editImportFilePath(launcher) }
-        }
-    }
-
-    private fun editImportFilePath(launcher: ManagedActivityResultLauncher<Array<String>, Uri?>) {
-        launcher.launch(VCF_MIME_TYPES)
     }
 }
---
+++ app/src/main/java/ch/abwesend/privatecontacts/view/screens/importexport/extensions/ImportExportConstants.kt
@@ -0,0 +1,13 @@
+/*
+ * Private Contacts
+ * Copyright (c) 2023.
+ * Florian Gubler
+ */
+
+package ch.abwesend.privatecontacts.view.screens.importexport.extensions
+
+object ImportExportConstants {
+    const val VCF_MAIN_MIME_TYPE = "text/x-vcard"
+    const val VCF_FILE_EXTENSION = "vcf"
+    val VCF_MIME_TYPES = arrayOf("text/vcard", VCF_MAIN_MIME_TYPE)
+}
