f1d04379fdeac897b23473b005bb230262312467
Florian Gubler <florian.gubler@ergon.ch>
2023-06-16 15:46:29 +0200

    Extracted logic for expandable card

--- app/src/main/java/ch/abwesend/privatecontacts/view/screens/contactedit/components/ContactEditCommonComponents.kt
+++ app/src/main/java/ch/abwesend/privatecontacts/view/screens/contactedit/components/ContactEditCommonComponents.kt
@@ -7,21 +7,15 @@
 package ch.abwesend.privatecontacts.view.screens.contactedit.components
 
 import androidx.annotation.StringRes
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
-import androidx.compose.material.Card
 import androidx.compose.material.Icon
+import androidx.compose.material.Surface
 import androidx.compose.material.Text
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.ExpandLess
-import androidx.compose.material.icons.filled.ExpandMore
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -31,7 +25,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
-import ch.abwesend.privatecontacts.R
+import ch.abwesend.privatecontacts.view.components.ExpandableCard
 import ch.abwesend.privatecontacts.view.theme.AppColors
 
 object ContactEditCommonComponents {
@@ -51,30 +45,21 @@ object ContactEditCommonComponents {
         content: @Composable () -> Unit
     ) {
         var expanded by remember { mutableStateOf(initiallyExpanded) }
-        val onExpandedChanged: (Boolean) -> Unit = { expanded = it }
 
-        Card(modifier = modifier.padding(all = 5.dp)) {
-            Box(modifier = Modifier.padding(horizontal = 5.dp, vertical = 10.dp)) {
-                Column {
-                    ContactCategoryHeader(
-                        title = categoryTitle,
-                        icon = icon,
-                        expanded = expanded,
-                        onToggleExpanded = { onExpandedChanged(!expanded) }
-                    )
-                    if (expanded) {
-                        Spacer(modifier = Modifier.height(10.dp))
-                        Row(modifier = Modifier.fillMaxWidth()) {
-                            if (alignContentWithTitle) {
-                                Spacer(modifier = primaryIconModifier.padding(end = iconHorizontalPadding))
-                            } else {
-                                Spacer(modifier = Modifier.padding(end = 5.dp))
-                            }
-                            Box(modifier = Modifier.padding(end = 5.dp)) {
-                                content()
-                            }
-                        }
-                    }
+        ExpandableCard(
+            expanded = expanded,
+            modifier = modifier,
+            onToggleExpanded = { expanded = it },
+            header = { ContactCategoryHeader(title = categoryTitle, icon = icon) }
+        ) {
+            Row(modifier = Modifier.fillMaxWidth()) {
+                if (alignContentWithTitle) {
+                    Spacer(modifier = primaryIconModifier.padding(end = iconHorizontalPadding))
+                } else {
+                    Spacer(modifier = Modifier.padding(end = 5.dp))
+                }
+                Surface(modifier = Modifier.padding(end = 5.dp)) {
+                    content()
                 }
             }
         }
@@ -84,16 +69,8 @@ object ContactEditCommonComponents {
     fun ContactCategoryHeader(
         @StringRes title: Int,
         icon: ImageVector,
-        expanded: Boolean,
-        onToggleExpanded: () -> Unit,
     ) {
-        val expandIcon = if (expanded) Icons.Default.ExpandLess else Icons.Default.ExpandMore
-
-        Row(
-            modifier = Modifier
-                .fillMaxWidth()
-                .clickable { onToggleExpanded() }
-        ) {
+        Row {
             Icon(
                 imageVector = icon,
                 contentDescription = stringResource(id = title),
@@ -103,11 +80,6 @@ object ContactEditCommonComponents {
             Column(modifier = Modifier.weight(1.0f)) {
                 Text(text = stringResource(id = title))
             }
-            Icon(
-                imageVector = expandIcon,
-                contentDescription = stringResource(id = R.string.expand),
-                modifier = secondaryIconModifier.padding(start = iconHorizontalPadding)
-            )
         }
     }
 }
