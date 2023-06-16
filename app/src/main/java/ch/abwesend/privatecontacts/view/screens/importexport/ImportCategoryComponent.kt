ba5c317d59c7fb8eeb49f1013b82ab5bb4a4a4f6
Florian Gubler <florian.gubler@ergon.ch>
2023-06-16 14:19:02 +0200

    Request the android contacts permission when importing into public contacts.
    And show a nice error-message if denied

--- app/src/main/java/ch/abwesend/privatecontacts/view/screens/importexport/ImportCategoryComponent.kt
+++ app/src/main/java/ch/abwesend/privatecontacts/view/screens/importexport/ImportCategoryComponent.kt
@@ -14,23 +14,30 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.Divider
 import androidx.compose.material.ExperimentalMaterialApi
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import ch.abwesend.privatecontacts.R
+import ch.abwesend.privatecontacts.domain.lib.logging.logger
 import ch.abwesend.privatecontacts.domain.model.contact.ContactAccount
-import ch.abwesend.privatecontacts.domain.model.contact.ContactType.PUBLIC
-import ch.abwesend.privatecontacts.domain.model.contact.ContactType.SECRET
+import ch.abwesend.privatecontacts.domain.model.contact.ContactType
 import ch.abwesend.privatecontacts.domain.model.importexport.ContactImportData
 import ch.abwesend.privatecontacts.domain.model.importexport.VCardParseError
 import ch.abwesend.privatecontacts.domain.model.result.generic.BinaryResult
 import ch.abwesend.privatecontacts.domain.model.result.generic.ErrorResult
 import ch.abwesend.privatecontacts.domain.model.result.generic.SuccessResult
+import ch.abwesend.privatecontacts.domain.util.injectAnywhere
 import ch.abwesend.privatecontacts.view.components.buttons.SecondaryButton
 import ch.abwesend.privatecontacts.view.components.dialogs.OkDialog
 import ch.abwesend.privatecontacts.view.components.dialogs.ResourceFlowProgressAndResultDialog
@@ -38,15 +45,19 @@ import ch.abwesend.privatecontacts.view.components.dialogs.SimpleProgressDialog
 import ch.abwesend.privatecontacts.view.components.inputs.AccountSelectionDropDownField
 import ch.abwesend.privatecontacts.view.components.inputs.ContactTypeField
 import ch.abwesend.privatecontacts.view.components.inputs.OpenFileFilePicker
+import ch.abwesend.privatecontacts.view.permission.AndroidContactPermissionHelper
 import ch.abwesend.privatecontacts.view.screens.importexport.ImportExportScreenComponents.ImportExportCategory
 import ch.abwesend.privatecontacts.view.screens.importexport.extensions.ImportExportConstants.VCF_MIME_TYPES
 import ch.abwesend.privatecontacts.view.screens.importexport.extensions.getFilePathForDisplay
+import ch.abwesend.privatecontacts.view.util.accountSelectionRequired
 import ch.abwesend.privatecontacts.view.viewmodel.ContactImportViewModel
 import kotlin.contracts.ExperimentalContracts
 
 @ExperimentalMaterialApi
 @ExperimentalContracts
 object ImportCategoryComponent {
+    private val contactPermissionHelper: AndroidContactPermissionHelper by injectAnywhere()
+
     private val parent = ContactImportExportScreen // TODO remove once google issue 212091796 is fixed
 
     @Composable
@@ -65,33 +76,11 @@ object ImportCategoryComponent {
 
             Spacer(modifier = Modifier.height(10.dp))
 
-            ContactTypeField(
-                labelRes = R.string.import_contacts_as,
-                selectedType = targetType,
-                isScrolling = { parent.isScrolling }
-            ) { newType -> viewModel.selectTargetType(newType) }
-
-            when (targetType) {
-                SECRET -> Unit
-                PUBLIC -> {
-                    Spacer(modifier = Modifier.height(5.dp))
-                    AccountSelectionDropDownField(defaultAccount = defaultAccount) { newValue ->
-                        viewModel.selectTargetAccount(newValue)
-                    }
-                }
-            }
+            TargetTypeFields(viewModel, targetType, selectedAccount)
 
             Spacer(modifier = Modifier.height(10.dp))
 
-            SecondaryButton(
-                enabled = viewModel.fileUri.value != null,
-                onClick = { viewModel.importContacts(targetType, selectedAccount) },
-            ) {
-                Text(
-                    text = stringResource(id = R.string.import_contacts),
-                    textAlign = TextAlign.Center
-                )
-            }
+            ImportButton(viewModel, targetType, selectedAccount)
         }
 
         ProgressAndResultHandler(viewModel = viewModel)
@@ -108,6 +97,73 @@ object ImportCategoryComponent {
     }
 
     @Composable
+    private fun TargetTypeFields(
+        viewModel: ContactImportViewModel,
+        targetType: ContactType,
+        selectedAccount: ContactAccount,
+    ) {
+        ContactTypeField(
+            labelRes = R.string.import_contacts_as,
+            selectedType = targetType,
+            isScrolling = { parent.isScrolling }
+        ) { newType -> viewModel.selectTargetType(newType) }
+
+        if (targetType.accountSelectionRequired) {
+            Spacer(modifier = Modifier.height(5.dp))
+            AccountSelectionDropDownField(selectedAccount = selectedAccount) { newValue ->
+                viewModel.selectTargetAccount(newValue)
+            }
+        }
+    }
+
+    @Composable
+    private fun ImportButton(
+        viewModel: ContactImportViewModel,
+        targetType: ContactType,
+        selectedAccount: ContactAccount,
+    ) {
+        var showPermissionDeniedDialog: Boolean by remember { mutableStateOf(false) }
+
+        SecondaryButton(
+            enabled = viewModel.fileUri.value != null,
+            onClick = {
+                importContacts(viewModel, targetType, selectedAccount) {
+                    showPermissionDeniedDialog = true
+                }
+            },
+        ) {
+            Text(
+                text = stringResource(id = R.string.import_contacts),
+                textAlign = TextAlign.Center
+            )
+        }
+
+        if (showPermissionDeniedDialog) {
+            PermissionDeniedDialog { showPermissionDeniedDialog = false }
+        }
+    }
+
+    private fun importContacts(
+        viewModel: ContactImportViewModel,
+        targetType: ContactType,
+        selectedAccount: ContactAccount,
+        onPermissionDenied: () -> Unit,
+    ) {
+        if (targetType.androidPermissionRequired) {
+            contactPermissionHelper.requestAndroidContactPermissions { result ->
+                logger.debug("Android contact permissions: $result")
+                if (result.usable) {
+                    viewModel.importContacts(targetType, selectedAccount)
+                } else {
+                    onPermissionDenied()
+                }
+            }
+        } else {
+            viewModel.importContacts(targetType, selectedAccount)
+        }
+    }
+
+    @Composable
     private fun ProgressAndResultHandler(viewModel: ContactImportViewModel) {
         ResourceFlowProgressAndResultDialog(
             flow = viewModel.importResult,
@@ -115,6 +171,25 @@ object ImportCategoryComponent {
             ProgressDialog = { ProgressDialog() },
             ResultDialog = { result, onClose -> ResultDialog(result, onClose) },
         )
+    }
+
+    @Composable
+    private fun PermissionDeniedDialog(closeDialog: () -> Unit) {
+        OkDialog(
+            title = R.string.import_export_permission_required_title,
+            onClose = closeDialog
+        ) {
+            Column {
+                Text(text = stringResource(id = R.string.import_export_permission_required_explanation))
+                Spacer(modifier = Modifier.height(10.dp))
+                Divider()
+                Spacer(modifier = Modifier.height(10.dp))
+                Text(
+                    text = stringResource(id = R.string.settings_info_dialog_android_contacts_permission),
+                    fontStyle = FontStyle.Italic,
+                )
+            }
+        }
     }
 
     @Composable
