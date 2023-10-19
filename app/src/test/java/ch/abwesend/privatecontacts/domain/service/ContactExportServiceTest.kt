4611957b45a8bc607c8b3d2c2ca087f44d3589e7
Florian Gubler <florian.gubler@ergon.ch>
2023-10-19 16:09:58 +0200

    Added tests for export-service

--- app/src/test/java/ch/abwesend/privatecontacts/domain/service/ContactExportServiceTest.kt
+++ app/src/test/java/ch/abwesend/privatecontacts/domain/service/ContactExportServiceTest.kt
@@ -8,11 +8,13 @@ package ch.abwesend.privatecontacts.domain.service
 
 import ch.abwesend.privatecontacts.domain.model.contact.ContactType
 import ch.abwesend.privatecontacts.domain.model.importexport.VCardCreateError
+import ch.abwesend.privatecontacts.domain.model.importexport.VCardCreateError.VCF_SERIALIZATION_FAILED
 import ch.abwesend.privatecontacts.domain.model.importexport.VCardVersion
 import ch.abwesend.privatecontacts.domain.model.result.generic.ErrorResult
 import ch.abwesend.privatecontacts.domain.model.result.generic.SuccessResult
 import ch.abwesend.privatecontacts.domain.service.interfaces.IVCardImportExportRepository
 import ch.abwesend.privatecontacts.testutil.TestBase
+import ch.abwesend.privatecontacts.testutil.databuilders.someContactEditable
 import ch.abwesend.privatecontacts.testutil.databuilders.someCreatedVCards
 import ch.abwesend.privatecontacts.testutil.databuilders.someUri
 import io.mockk.coEvery
@@ -66,87 +68,39 @@ class ContactExportServiceTest : TestBase() {
         assertThat(result.getErrorOrNull()).isEqualTo(VCardCreateError.FILE_WRITING_FAILED)
     }
 
-// TODO adapt / implement
-//
-//    @Test
-//    fun `should forward error from vcf-parsing if that fails`() {
-//        val file = someUri()
-//        val targetType = ContactType.SECRET
-//        val targetAccount = ContactAccount.None
-//        val fileContent = someFileContent()
-//        val error = VCF_PARSING_FAILED
-//        coEvery { fileReadService.readFileContent(any()) } returns SuccessResult(fileContent)
-//        coEvery { importExportRepository.parseContacts(any(), any()) } returns ErrorResult(error)
-//
-//        val result = runBlocking { underTest.importContacts(file, targetType, targetAccount) }
-//
-//        coVerify { importExportRepository.parseContacts(fileContent, targetType) }
-//        assertThat(result).isInstanceOf(ErrorResult::class.java)
-//        assertThat(result.getErrorOrNull()).isEqualTo(error)
-//    }
-//
-//    @Test
-//    fun `should set the targetType and targetAccount properly`() {
-//        val file = someUri()
-//        val targetType = ContactType.SECRET
-//        val targetAccount = ContactAccount.None
-//        val fileContent = someFileContent()
-//        val contact = someContactEditable(type = ContactType.PUBLIC, saveInAccount = LocalPhoneContacts)
-//        val parsedData = someParsedData(listOf(contact))
-//        coEvery { fileReadService.readFileContent(any()) } returns SuccessResult(fileContent)
-//        coEvery { importExportRepository.parseContacts(any(), any()) } returns SuccessResult(parsedData)
-//        coEvery { contactSaveService.saveContacts(any()) } answers {
-//            val contacts: List<IContactEditable> = firstArg()
-//            contacts.associateWith { ContactSaveResult.Success }
-//        }
-//
-//        val result = runBlocking { underTest.importContacts(file, targetType, targetAccount) }
-//
-//        coVerify { importExportRepository.parseContacts(fileContent, targetType) }
-//        assertThat(result).isInstanceOf(SuccessResult::class.java)
-//        val importedContacts = result.getValueOrNull()?.newImportedContacts
-//        assertThat(importedContacts).isNotNull.hasSize(1)
-//        assertThat(importedContacts!!.first().type).isEqualTo(targetType)
-//        assertThat(importedContacts.first().saveInAccount).isEqualTo(targetAccount)
-//    }
-//
-//    @Test
-//    fun `should save contacts and return save-results`() {
-//        val file = someUri()
-//        val targetType = ContactType.SECRET
-//        val targetAccount = ContactAccount.None
-//        val fileContent = someFileContent()
-//        val contacts = listOf(
-//            someContactEditable(firstName = "A", type = ContactType.PUBLIC, saveInAccount = LocalPhoneContacts),
-//            someContactEditable(firstName = "B", type = ContactType.PUBLIC, saveInAccount = LocalPhoneContacts),
-//            someContactEditable(firstName = "C", type = ContactType.PUBLIC, saveInAccount = LocalPhoneContacts),
-//        )
-//        val error = UNKNOWN_ERROR
-//        val validationError = NAME_NOT_SET
-//        val parsedData = someParsedData(successfulContacts = contacts)
-//        coEvery { fileReadService.readFileContent(any()) } returns SuccessResult(fileContent)
-//        coEvery { importExportRepository.parseContacts(any(), any()) } returns SuccessResult(parsedData)
-//        coEvery { contactSaveService.saveContacts(any()) } answers {
-//            val savingContacts: List<IContactEditable> = firstArg()
-//            mapOf(
-//                savingContacts[0] to ContactSaveResult.Success,
-//                savingContacts[1] to ContactSaveResult.Failure(error),
-//                savingContacts[2] to ContactSaveResult.ValidationFailure(listOf(validationError)),
-//            )
-//        }
-//
-//        val result = runBlocking { underTest.importContacts(file, targetType, targetAccount) }
-//
-//        coVerify { contactSaveService.saveContacts(contacts) }
-//        assertThat(result).isInstanceOf(SuccessResult::class.java)
-//        val resultData = result.getValueOrNull()!!
-//        assertThat(resultData.newImportedContacts).hasSize(1)
-//        assertThat(resultData.importFailures).hasSize(1)
-//        assertThat(resultData.importValidationFailures).hasSize(1)
-//        assertThat(resultData.newImportedContacts.first()).isEqualTo(contacts[0])
-//        assertThat(resultData.importFailures).containsKey(contacts[1])
-//        assertThat(resultData.importFailures[contacts[1]]).containsExactly(error)
-//        assertThat(resultData.importValidationFailures).containsKey(contacts[2])
-//        assertThat(resultData.importValidationFailures[contacts[2]]).containsExactly(validationError)
-//    }
+    @Test
+    fun `should return error if vcf creation fails`() {
+        val file = someUri()
+        val sourceType = ContactType.SECRET
+        val vCardVersion = VCardVersion.V3
+        val contact = someContactEditable()
+        coEvery { contactLoadService.loadFullContactsByType(any()) } returns listOf(contact)
+        coEvery { importExportRepository.exportContacts(any(), any()) } returns ErrorResult(VCF_SERIALIZATION_FAILED)
+
+        val result = runBlocking { underTest.exportContacts(file, sourceType, vCardVersion) }
+
+        coVerify { importExportRepository.exportContacts(listOf(contact), vCardVersion) }
+        assertThat(result).isInstanceOf(ErrorResult::class.java)
+        assertThat(result.getErrorOrNull()).isEqualTo(VCF_SERIALIZATION_FAILED)
+    }
+
+    @Test
+    fun `should return success if export succeeds`() {
+        val file = someUri()
+        val sourceType = ContactType.SECRET
+        val vCardVersion = VCardVersion.V3
+        val contact = someContactEditable()
+        val partialData = someCreatedVCards()
+        coEvery { contactLoadService.loadFullContactsByType(any()) } returns listOf(contact)
+        coEvery { importExportRepository.exportContacts(any(), any()) } returns SuccessResult(partialData)
+        coEvery { fileWriteService.writeContentToFile(any(), any()) } returns SuccessResult(Unit)
+
+        val result = runBlocking { underTest.exportContacts(file, sourceType, vCardVersion) }
+
+        coVerify { importExportRepository.exportContacts(listOf(contact), vCardVersion) }
+        coVerify { fileWriteService.writeContentToFile(partialData.fileContent, file) }
+        assertThat(result).isInstanceOf(SuccessResult::class.java)
+        assertThat(result.getValueOrNull()!!.failedContacts).isEqualTo(partialData.failedContacts)
+        assertThat(result.getValueOrNull()!!.successfulContacts).isEqualTo(listOf(contact))
+    }
 }
