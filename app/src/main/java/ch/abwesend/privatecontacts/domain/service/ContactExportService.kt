6903cecb6253c0e1d8fc354e09ce11352d59f32d
Florian Gubler <florian.gubler@ergon.ch>
2023-12-28 13:49:07 +0100

    Small refactoring

--- app/src/main/java/ch/abwesend/privatecontacts/domain/service/ContactExportService.kt
+++ app/src/main/java/ch/abwesend/privatecontacts/domain/service/ContactExportService.kt
@@ -9,6 +9,7 @@ package ch.abwesend.privatecontacts.domain.service
 import android.net.Uri
 import ch.abwesend.privatecontacts.domain.lib.coroutine.IDispatchers
 import ch.abwesend.privatecontacts.domain.model.contact.ContactType
+import ch.abwesend.privatecontacts.domain.model.contact.IContact
 import ch.abwesend.privatecontacts.domain.model.importexport.ContactExportData
 import ch.abwesend.privatecontacts.domain.model.importexport.VCardCreateError
 import ch.abwesend.privatecontacts.domain.model.importexport.VCardCreateError.FILE_WRITING_FAILED
@@ -31,7 +32,14 @@ class ContactExportService {
         vCardVersion: VCardVersion,
     ): BinaryResult<ContactExportData, VCardCreateError> = withContext(dispatchers.default) {
         val contacts = loadService.loadFullContactsByType(sourceType)
+        exportContacts(targetFile, vCardVersion, contacts)
+    }
 
+    suspend fun exportContacts(
+        targetFile: Uri,
+        vCardVersion: VCardVersion,
+        contacts: List<IContact>,
+    ): BinaryResult<ContactExportData, VCardCreateError> = withContext(dispatchers.default) {
         val vCardResult = importExportRepository.exportContacts(contacts, vCardVersion)
 
         val fileWriteResult = vCardResult.mapValueToBinaryResult { createdVCards ->
