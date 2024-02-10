db76660b8fa2d11121b14279c07c1fd38fc4064d
Florian Gubler <florian.gubler@gmail.com>
2024-02-10 09:57:54 +0100

    Added workaround for company-only contacts from contact-store

--- app/src/main/java/ch/abwesend/privatecontacts/infrastructure/repository/androidcontacts/mapping/AndroidContactMapper.kt
+++ app/src/main/java/ch/abwesend/privatecontacts/infrastructure/repository/androidcontacts/mapping/AndroidContactMapper.kt
@@ -8,6 +8,7 @@ import ch.abwesend.privatecontacts.domain.model.contact.ContactIdAndroid
 import ch.abwesend.privatecontacts.domain.model.contact.ContactType
 import ch.abwesend.privatecontacts.domain.model.contact.IContact
 import ch.abwesend.privatecontacts.domain.model.contact.IContactBase
+import ch.abwesend.privatecontacts.domain.model.contact.IContactEditable
 import ch.abwesend.privatecontacts.domain.model.contactdata.Company
 import ch.abwesend.privatecontacts.domain.util.injectAnywhere
 import com.alexstyl.contactstore.Contact
@@ -48,20 +49,37 @@ class AndroidContactMapper {
                 contactDataSet = contactDataFactory.getContactData(contact).toMutableList(),
                 contactGroups = groups.toContactGroups().toMutableList(),
                 saveInAccount = ContactAccount.None,
-            ).also { contactEditable ->
-                // TODO consider removing this once proper company-support is added
-                val numberOfCompanies = contactEditable.contactDataSet.count { it is Company }
-                if (organization.isNotEmpty()) {
-                    val companyName = if (jobTitle.isBlank()) organization else "$organization ($jobTitle)"
-                    val companyFromOrganisation = Company.createEmpty(sortOrder = numberOfCompanies)
-                        .copy(value = companyName)
-                    contactEditable.contactDataSet.add(companyFromOrganisation)
-                }
-            }
+            ).also { it.addCompanyWorkarounds(this) }
         } catch (t: Throwable) {
             logger.warning("Failed to map android contact with id = $contactId", t)
             if (rethrowExceptions) throw t
             else null
+        }
+    }
+
+    /**
+     * Contacts without first-/last-name are not allowed.
+     * Workaround for nickname but also companies.
+     * TODO consider removing this (partially?) once proper company-support is added
+     */
+    private fun IContactEditable.addCompanyWorkarounds(androidContact: Contact) {
+        if (firstName.isEmpty() && lastName.isEmpty()) {
+            if (nickname.isNotEmpty()) {
+                firstName = nickname
+            } else if (androidContact.organization.isNotEmpty()) {
+                // temporary workaround: this should actually be stored in a field like "organizationName"
+                firstName = androidContact.organization
+            }
+        }
+
+        val numberOfCompanies = contactDataSet.count { it is Company }
+        if (androidContact.organization.isNotEmpty()) {
+            val companyName = with(androidContact) {
+                if (jobTitle.isBlank()) organization else "$organization ($jobTitle)"
+            }
+            val companyFromOrganisation = Company.createEmpty(sortOrder = numberOfCompanies)
+                .copy(value = companyName)
+            contactDataSet.add(companyFromOrganisation)
         }
     }
 }
