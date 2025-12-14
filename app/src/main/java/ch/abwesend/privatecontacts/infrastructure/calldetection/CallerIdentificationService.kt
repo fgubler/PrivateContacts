685585d6b5a12e339d51a947b9df6f47ffac3afd
Florian Gubler <florian.gubler@gmail.com>
2025-12-14 10:58:31 +0100

    Do not block calls if the app does not have the permission to read phone contacts
    If the public contacts cannot be considered, the blocking will be far too restrictive...

--- app/src/main/java/ch/abwesend/privatecontacts/infrastructure/calldetection/CallerIdentificationService.kt
+++ app/src/main/java/ch/abwesend/privatecontacts/infrastructure/calldetection/CallerIdentificationService.kt
@@ -15,10 +15,12 @@ import ch.abwesend.privatecontacts.domain.lib.logging.logger
 import ch.abwesend.privatecontacts.domain.settings.Settings
 import ch.abwesend.privatecontacts.domain.util.applicationScope
 import ch.abwesend.privatecontacts.domain.util.injectAnywhere
+import ch.abwesend.privatecontacts.domain.service.interfaces.PermissionService
 import kotlinx.coroutines.launch
 
 class CallerIdentificationService : CallScreeningService() {
     private val incomingCallHelper: IncomingCallHelper by injectAnywhere()
+    private val permissionService: PermissionService by injectAnywhere()
     private val context: Context = this
 
     override fun onScreenCall(callDetails: Details) {
@@ -42,8 +44,11 @@ class CallerIdentificationService : CallScreeningService() {
         logger.debug("Incoming call from number '$phoneNumber'")
 
         val blockCallsFromUnknownNumbers = Settings.nextOrDefault().blockIncomingCallsFromUnknownNumbers
+        val hasReadContactsPermission = permissionService.hasContactReadPermission()
+
         val shouldBlockCall = phoneNumber != null &&
             blockCallsFromUnknownNumbers &&
+            hasReadContactsPermission &&
             !incomingCallHelper.matchesAnyKnownContact(phoneNumber)
 
         if (shouldBlockCall) {
