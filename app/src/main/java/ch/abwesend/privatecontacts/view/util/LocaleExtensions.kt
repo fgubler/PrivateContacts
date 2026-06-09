8ebed1bbcbf8de500f63afb2c12c616cc4375f1b
Florian <florian.gubler@gmail.com>
2026-06-09 22:04:00 +0200

    Fixed bug from crashlytics
    Fatal Exception: java.lang.IllegalStateException: Can't change activity type once set: { mBounds=Rect(0, 0 - 1080, 2460) mAppBounds=Rect(0, 104 - 1080, 2330) mMaxBounds=Rect(0, 0 - 1080, 2460) mDisplayRotation=ROTATION_0 mWindowingMode=fullscreen mDisplayWindowingMode=fullscreen mActivityType=standard mAlwaysOnTop=undefined mRotation=ROTATION_0 mInSplitScreen=false letterBoxed=false} activityType=home

--- app/src/main/java/ch/abwesend/privatecontacts/view/util/LocaleExtensions.kt
+++ app/src/main/java/ch/abwesend/privatecontacts/view/util/LocaleExtensions.kt
@@ -5,17 +5,23 @@ import android.content.Context
 import android.os.Build
 import android.os.LocaleList
 import androidx.core.content.getSystemService
+import ch.abwesend.privatecontacts.domain.lib.logging.logger
 import ch.abwesend.privatecontacts.domain.settings.AppLanguage
 
 fun Context.tryChangeAppLanguage(language: AppLanguage) {
     if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
         getSystemService<LocaleManager>()?.let { localeManager ->
-            val newLocaleList = if (language.locale == null) {
-                localeManager.systemLocales
-            } else {
-                LocaleList.forLanguageTags(language.locale.language)
+            val newLocaleList = language.locale
+                ?.let { LocaleList.forLanguageTags(it.language) }
+                ?: localeManager.systemLocales
+
+            if (localeManager.applicationLocales != newLocaleList) {
+                try {
+                    localeManager.applicationLocales = newLocaleList
+                } catch (e: IllegalStateException) {
+                    logger.warning("Failed to change the application locales", e)
+                }
             }
-            localeManager.applicationLocales = newLocaleList
         }
     }
 }
